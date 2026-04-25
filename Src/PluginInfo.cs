4a0eb43bbdb2291e2fcffd8260677ae24697d9d9
Lexi <reeseivey21@hotmail.com>
2026-04-25 14:06:28 -0400

    Delete Src directory

--- Src/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CerealMenu
-{
-    internal class PluginInfo
-    {
-        public const string GUID = "Lexi.Cereal.Menu";
-        public const string Name = "Cereal.Menu";
-        public const string Version = "1.0.7";
-    }
-}
